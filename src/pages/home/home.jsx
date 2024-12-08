319190a198df31b5f25edbc6e7b43eab6c8257a6
DiegoCanales <diegocanales112@gmail.com>
2024-12-07 21:22:25 -0300

    index finished on mobile

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Navbar from '../../components/navbarIndex/navbarIndex.jsx'
 import Footer from '../../components/footer/footer.jsx'
 import logo from "../../assets/logopngtalvez.png";
+import iconClinic from "../../assets/svg/clinic.svg"
+import iconPresentacion from "../../assets/svg/presentaciones.svg"
+import iconClase from "../../assets/svg/clases.svg"
+import iconPsicologo from "../../assets/svg/psicologo.svg"
+import iconBoxeo from "../../assets/svg/boxeo.svg"
+import iconCancha from "../../assets/svg/cancha.svg"
+import imgSobreNosotros from "../../assets/img/sobrenosotros.jpg"
 import './home.css';
 
 export const home = () => {
@@ -18,6 +25,75 @@ export const home = () => {
 					<h1 className='tituloHome'>¡Reserva tu turno en segundos!</h1>
 					<i class="bi bi-arrow-down-short"></i>
 				</div>
+				<div className='fondoGradiente'>
+					<h2 className="tituloService">Servicios</h2>
+					<div className='contenedorServices'>
+						<a href="">
+							<div className="service">
+								<img src={iconClinic} alt="" />
+								<h3>Consultas Medicas</h3>
+							</div>
+						</a>
+						<a href="">
+							<div className="service">
+								<img src={iconPresentacion} alt="" />
+								<h3>Conferencias</h3>
+							</div>
+						</a>
+						<a href="">
+							<div className="service">
+								<img src={iconClase} alt="" />
+								<h3>Clases de Consulta</h3>
+							</div>
+						</a>
+						<a href="">
+							<div className="service">
+								<img src={iconPsicologo} alt="" />
+								<h3>Psicologo</h3>
+							</div>
+						</a>
+						<a href="">
+							<div className="service">
+								<img src={iconBoxeo} alt="" />
+								<h3>Clases de Boxeo</h3>
+							</div>
+						</a>
+						<a href="">
+							<div className="service">
+								<img src={iconCancha} alt="" />
+								<h3>Canchas</h3>
+							</div>
+						</a>
+					</div>
+
+					<h2 className="tituloSobreNosotros">Sobre Nosotros</h2>
+					<div className='contSobreNosotros'>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quaerat totam, quis molestias facere dolores id aperiam perferendis officia consequuntur?</p>
+						<img src={imgSobreNosotros} alt="" />
+					</div>
+
+
+					<h2 className='tituloSobreNosotros'>Contactanos</h2>
+					<p className='textoContactanos'>Envianos tu consulta y nos comunicaremos a la brevedad</p>
+
+					<form action="" className='formularioIndex'>
+						<label><input type="text" placeholder='Nombre' name="" /></label>
+
+						<label><input type="email" placeholder='Correo' name="" /></label>
+
+						<label><textarea name="" placeholder='Ingresa tu mensaje...'></textarea></label>
+
+						<button type="submit">Enviar</button>
+					</form>
+					
+					
+
+
+
+
+				</div>
+				
+				
 				<Footer></Footer>
 		</>
 	)
