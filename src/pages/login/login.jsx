f82b7969c9062ba9e230adf39b02e50730a19c95
Alvaro Rubina <alvarorubina132@gmail.com>
2024-12-09 03:38:41 -0300

    Añadido formulario de registro

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,42 +3,74 @@ import logoGoogle from '../../assets/svg/logoGoogle.svg'
 import './login.css'
 
 const login = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-    const openModal = () => {
-        setIsModalOpen(true)
-    }
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
 
-    const closeModal = () => {
-        setIsModalOpen(false)
-    }
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
 
   return (
     <>
-        <div className='contenedor-login'>
-            <h1 className='titulo-login'>INICIÁ SESIÓN</h1>
-            <div className='formularios-login'>
-                <input className='input-login' type="text" placeholder="Ingresá tu email"/>
-                <input className='input-login' type="password" placeholder="Ingresá tu contraseña"/>
-            </div>
-            <div className='contenedor-botones-login'>
-                <button className='boton-login'>INICIAR SESIÓN</button>
-                <button className='boton-login-google'>
-                    INICIAR SESIÓN CON GOOGLE
-                    <img src={logoGoogle} alt="Google logo" className='logo-google' />
-                </button>
-                <button className='boton-login-registrar' onClick={openModal}>REGISTRATE</button>
-            </div>
-        </div>
+      <div className='contenedor-login'>
+        <h1 className='titulo-login'>INICIÁ SESIÓN</h1>
+        <form className='formulario-login' action="#">
+          <input className='input-login' type="email" placeholder="Ingresá tu email" required/>
+          <input className='input-login' type="password" placeholder="Ingresá tu contraseña" required/>
 
-        {isModalOpen && (
-            <div className='modal-registro'>
-                <div className='contenido-modal'>
-                    <span className='close-modal' onClick={closeModal}>&times;</span>
-                    <h1 className='titulo-modal'>REGISTRATE</h1>
-                </div>
+          <div className='contenedor-botones-login'>
+            <button className='boton-login'>INICIAR SESIÓN</button>
+            <button className='boton-login-google' onClick={(e) => e.preventDefault()}>
+              INICIAR SESIÓN CON GOOGLE
+              <img src={logoGoogle} alt="Google logo" className='logo-google' />
+            </button>
+            <button className='boton-login-registrar' onClick={(e) => { e.preventDefault(); openModal(); }}>
+              REGISTRATE
+            </button>
+          </div>
+        </form>
+        
+        
+      </div>
+
+      {isModalOpen && (
+        <div className='modal-registro'>
+          <div className="contenido-modal-registro">
+            <span className='close-modal-registro' onClick={closeModal}>&times;</span>
+            <div className="cabecera-modal-registro">
+              <h2>Formulario de registro</h2>
+              <p>Blablablablablablabla.</p>
             </div>
-        )}
+            {/* No se que esto de FORM ACTION="#" */}
+            <form className='formulario-registro' action="#">
+              <h3>Nombre:</h3>
+              <input type="text" placeholder="Ingresá tu nombre" required />
+              <h3>Apellido:</h3>
+              <input type="text" placeholder="Ingresá tu apellido" required />
+              <h3>Fecha de nacimiento:</h3>
+              <input type="date" placeholder="Fecha de nacimiento" required />
+              <h3>Correo electrónico:</h3>
+              <input type="email" placeholder="ejemplo@ejemplo.com" required />
+              <h3>Contraseña:</h3>
+              <input type="password" placeholder="Ingresá una contraseña segura" required />
+              <h3>Repetir contraseña:</h3>
+              <input type="password" placeholder="Repetí la contraseña" required />
+
+              <div className='contenedor-botones-login'>
+                <button className='boton-login-google' onClick={(e) => e.preventDefault()}>
+                  REGISTRARSE CON GOOGLE
+                  <img src={logoGoogle} alt="Google logo" className='logo-google' />
+                </button>
+                <button className='boton-login'>REGISTRARSE</button>
+              </div>
+              <span> Ya tenés una cuenta? <a href="#" onClick={closeModal}>Iniciá sesión</a></span>
+            </form>
+          </div>
+        </div>
+      )}
     </>
   )
 }
