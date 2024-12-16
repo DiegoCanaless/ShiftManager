adb15bb40a8e9cbf4eb78f72ece0853db73539c7
Alvaro Rubina <alvarorubina132@gmail.com>
2024-12-15 23:04:36 -0300

    ajustes en svg

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/home/home.jsx'
+import Login from './pages/login/Login.jsx'
+import Admin from './pages/admin/Admin.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home></Home>
+    {/* <Home></Home> */}
+    {/* <Login></Login> */}
+    <Admin></Admin>
   </StrictMode>,
 )
