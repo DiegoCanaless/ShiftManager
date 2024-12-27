c3455e428c9c8ee07ea809b3448c01df42fdc80e
Alvaro Rubina <alvarorubina132@gmail.com>
2024-12-27 17:25:17 -0300

    Añadido parametro style a Boton

--- src/components/boton/boton.jsx
+++ src/components/boton/boton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Boton.css'
 
-const Boton = ({text, onClick, className, children}) => {
+const Boton = ({text, onClick, className, children, style}) => {
   return (
-    <button className={`boton ${className}`} onClick={onClick}>
+    <button className={`boton ${className}`} onClick={onClick} style={style}>
       {text}
       {children}
     </button>
